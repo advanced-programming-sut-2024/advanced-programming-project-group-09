2a9a2360537cc86db207f32603257862ce237cd1
Parham <147194404+ParhamAI@users.noreply.github.com>
2024-06-30 21:20:56 +0330

    Updated User.java to resolve merge conflict

--- src/main/java/Sample/Model/User.java
+++ src/main/java/Sample/Model/User.java
@@ -5,14 +5,17 @@ import Sample.CardEnums.Faction;
 import Sample.CardEnums.Leader;
 import Sample.CardEnums.SpecialCard;
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
-import java.io.FileWriter;
+import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.HashMap;
-
+import java.util.Map;
 
 public class User {
     private String username;
@@ -24,9 +27,6 @@ public class User {
     private int draws;
     private int loses;
     private double maxScore;
-    private User competitor;
-    private GameBattleField lastGameBattleField;
-    private final ArrayList<GameBattleField> allGameBattleField = new ArrayList<>();
     private static final ArrayList<User> allUsers = new ArrayList<>();
     private static User userLoginIn;
     private HashMap<String, String> securityQuestionsAndAnswers = new HashMap<>();
@@ -36,6 +36,30 @@ public class User {
 
     private final ArrayList<CommonCard> commonCardsInDeck = new ArrayList<>();
     private final ArrayList<SpecialCard> specialCardsInDeck = new ArrayList<>();
+    private final ArrayList<CommonCard> commonCardInBattleField = new ArrayList<>();
+    private final ArrayList<SpecialCard> specialCardsBattleField = new ArrayList<>();
+    private final ArrayList<CommonCard> closeCombatBattleField = new ArrayList<>();
+    private final ArrayList<CommonCard> commonCardsInDiscard = new ArrayList<>();
+    private final ArrayList<SpecialCard> specialCardsDiscard = new ArrayList<>();
+    private final ArrayList<CommonCard> siegeIsPlayed = new ArrayList<>();
+    private final ArrayList<CommonCard> rangedIsPlayed = new ArrayList<>();
+    private SpecialCard specialFieldInCloseCombat = null;
+    private SpecialCard specialFieldInSiege = null;
+    private SpecialCard specialFieldInRanged = null;
+
+    private static final String DECKS_DIR = "decks/";
+    private static final Map<String, String> savedDecks = new HashMap<>();
+    private static final String FILE_PATH = "users.json";
+    private static final Gson gson = new Gson();
+
+    // Ensure the decks directory exists
+    static {
+        loadUsers();
+        File dir = new File(DECKS_DIR);
+        if (!dir.exists()) {
+            dir.mkdirs();
+        }
+    }
 
     public User(String username, String password, String email, String nickname) {
         this.username = username;
@@ -43,12 +67,12 @@ public class User {
         this.email = email;
         this.nickname = nickname;
         allUsers.add(this);
+        saveUsers(); // Save users whenever a new user is created
     }
 
     public static ArrayList<User> getUsers() {
         return allUsers;
     }
-
 
     public static User getUserByUsername(String username) {
         for (User user : allUsers) {
@@ -69,28 +93,36 @@ public class User {
 
     public void setFactionLeader(Leader factionLeader) {
         this.factionLeader = factionLeader;
+        saveUsers();
     }
 
     public Leader getFactionLeader() {
         return factionLeader;
     }
 
+    public ArrayList<SpecialCard> getSpecialCardsBattleField() {
+        return specialCardsBattleField;
+    }
+
     public void setUsername(String username) {
         this.username = username;
+        saveUsers();
     }
 
     public void setPassword(String password) {
         this.password = password;
+        saveUsers();
     }
 
     public void setEmail(String email) {
         this.email = email;
+        saveUsers();
     }
 
     public void setNickname(String nickname) {
         this.nickname = nickname;
+        saveUsers();
     }
-
 
     public String getUsername() {
         return username;
@@ -110,6 +142,7 @@ public class User {
 
     public void setSecurityQuestionsAndAnswers(HashMap<String, String> securityQuestionsAndAnswers) {
         this.securityQuestionsAndAnswers = securityQuestionsAndAnswers;
+        saveUsers();
     }
 
     public HashMap<String, String> getSecurityQuestionsAndAnswers() {
@@ -118,6 +151,22 @@ public class User {
 
     public void setFactionSelected(Faction factionSelected) {
         this.factionSelected = factionSelected;
+        saveUsers();
+    }
+
+    public void setSpecialFieldInCloseCombat(SpecialCard specialFieldInCloseCombat) {
+        this.specialFieldInCloseCombat = specialFieldInCloseCombat;
+        saveUsers();
+    }
+
+    public void setSpecialFieldInSiege(SpecialCard specialFieldInSiege) {
+        this.specialFieldInSiege = specialFieldInSiege;
+        saveUsers();
+    }
+
+    public void setSpecialFieldInRanged(SpecialCard specialFieldInRanged) {
+        this.specialFieldInRanged = specialFieldInRanged;
+        saveUsers();
     }
 
     public Faction getFactionSelected() {
@@ -132,20 +181,134 @@ public class User {
         return specialCardsInDeck;
     }
 
+    public ArrayList<CommonCard> getCommonCardInBattleField() {
+        return commonCardInBattleField;
+    }
+
+    public ArrayList<SpecialCard> getSpecialCardsCanPlay() {
+        return specialCardsBattleField;
+    }
+
+    public ArrayList<CommonCard> getCommonCardsInDiscard() {
+        return commonCardsInDiscard;
+    }
+
+    public ArrayList<SpecialCard> getSpecialCardsDiscard() {
+        return specialCardsDiscard;
+    }
+
+    public ArrayList<CommonCard> getCloseCombatBattleField() {
+        return closeCombatBattleField;
+    }
+
+    public ArrayList<CommonCard> getSiegeIsPlayed() {
+        return siegeIsPlayed;
+    }
+
+    public ArrayList<CommonCard> getRangedIsPlayed() {
+        return rangedIsPlayed;
+    }
+
+    public SpecialCard getSpecialFieldInCloseCombat() {
+        return specialFieldInCloseCombat;
+    }
+
+    public SpecialCard getSpecialFieldInSiege() {
+        return specialFieldInSiege;
+    }
+
+    public SpecialCard getSpecialFieldInRanged() {
+        return specialFieldInRanged;
+    }
+
     public void addCardToCommonCardsInDeck(CommonCard commonCard) {
         commonCardsInDeck.add(commonCard);
+        saveUsers();
+    }
+
+    public void addCardToCommonCardsInDiscard(CommonCard commonCard) {
+        commonCardsInDiscard.add(commonCard);
+        saveUsers();
+    }
+
+    public void addCardToCommonCardsInBattleField(CommonCard commonCard) {
+        commonCardInBattleField.add(commonCard);
+        saveUsers();
     }
 
     public void addCardToSpecialCardsInDeck(SpecialCard specialCard) {
         specialCardsInDeck.add(specialCard);
+        saveUsers();
+    }
+
+    public void addCardToSpecialCardsInDiscard(SpecialCard specialCard) {
+        specialCardsDiscard.add(specialCard);
+        saveUsers();
+    }
+
+    public void addCardToSpecialCardsInBattleField(SpecialCard specialCard) {
+        specialCardsBattleField.add(specialCard);
+        saveUsers();
+    }
+
+    public void addCardToCommonCardsInCloseCombat(CommonCard commonCard) {
+        closeCombatBattleField.add(commonCard);
+        saveUsers();
+    }
+
+    public void addCardToCommonCardsInSiege(CommonCard commonCard) {
+        siegeIsPlayed.add(commonCard);
+        saveUsers();
+    }
+
+    public void addCardToCommonCardsInRanged(CommonCard commonCard) {
+        closeCombatBattleField.add(commonCard);
+        saveUsers();
     }
 
     public void removeCardToCommonCardsInDeck(CommonCard commonCard) {
+        commonCardsInDeck.remove(commonCard);
+        saveUsers();
+    }
 
+    public void removeCardToCommonCardsInDiscard(CommonCard commonCard) {
+        commonCardsInDiscard.remove(commonCard);
+        saveUsers();
+    }
+
+    public void removeCardToCommonCardsInBattleField(CommonCard commonCard) {
+        commonCardInBattleField.remove(commonCard);
+        saveUsers();
     }
 
     public void removeCardToSpecialCardsInDeck(SpecialCard specialCard) {
+        specialCardsInDeck.remove(specialCard);
+        saveUsers();
+    }
 
+    public void removeCardToSpecialCardsInDiscard(SpecialCard specialCard) {
+        specialCardsDiscard.remove(specialCard);
+        saveUsers();
+    }
+
+    public void removeCardToSpecialCardsInBattleField(SpecialCard specialCard) {
+        specialCardsBattleField.remove(specialCard);
+        saveUsers();
+    }
+
+    public void removeCardToCommonCardsInCloseCombat(CommonCard commonCard) {
+        closeCombatBattleField.remove(commonCard);
+        saveUsers();
+    }
+
+    public void removeCardToCommonCardsInSiege(CommonCard commonCard) {
+        siegeIsPlayed.remove(commonCard);
+        saveUsers();
+    }
+
+    public void removeCardToCommonCardsInRanged(CommonCard commonCard) {
+        closeCombatBattleField.remove(commonCard);
+        saveUsers();
     }
 
     public double getMaxScore() {
@@ -154,6 +317,7 @@ public class User {
 
     public void setMaxScore(double maxScore) {
         this.maxScore = maxScore;
+        saveUsers();
     }
 
     public int getLoses() {
@@ -162,6 +326,7 @@ public class User {
 
     public void setLoses(int loses) {
         this.loses = loses;
+        saveUsers();
     }
 
     public int getDraws() {
@@ -170,6 +335,7 @@ public class User {
 
     public void setDraws(int draws) {
         this.draws = draws;
+        saveUsers();
     }
 
     public int getWins() {
@@ -178,6 +344,7 @@ public class User {
 
     public void setWins(int wins) {
         this.wins = wins;
+        saveUsers();
     }
 
     public int getGamesPlayed() {
@@ -186,6 +353,7 @@ public class User {
 
     public void setGamesPlayed(int gamesPlayed) {
         this.gamesPlayed = gamesPlayed;
+        saveUsers();
     }
 
     public int getRank() {
@@ -198,46 +366,76 @@ public class User {
         return allUsers.indexOf(this);
     }
 
-    public User getCompetitor() {
-        return competitor;
+    public static void saveUsers() {
+        try (FileWriter writer = new FileWriter(FILE_PATH)) {
+            gson.toJson(allUsers, writer);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    public void setCompetitor(User competitor) {
-        this.competitor = competitor;
+    public static void loadUsers() {
+        File file = new File(FILE_PATH);
+        if (!file.exists()) {
+            System.out.println("File does not exist.");
+            return;
+        }
+        try (FileReader reader = new FileReader(file)) {
+            Type type = new TypeToken<ArrayList<User>>() {
+            }.getType();
+            ArrayList<User> loadedUsers = gson.fromJson(reader, type);
+            allUsers.clear();
+            allUsers.addAll(loadedUsers);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    public GameBattleField getLastGameBattleField() {
-        return lastGameBattleField;
-    }
-
-    public void setLastGameBattleField(GameBattleField lastGameBattleField) {
-        this.lastGameBattleField = lastGameBattleField;
-    }
-
-    public ArrayList<GameBattleField> getAllGameBattleField() {
-        return allGameBattleField;
-    }
-
-    public void addToAllGameBattleField(GameBattleField gameBattleField) {
-        allGameBattleField.add(gameBattleField);
-    }
-
-    public void saveUserData(String filePath) {
-        Gson gson = new Gson();
-        try (FileWriter writer = new FileWriter(filePath)) {
+    public void saveDeckToFile(String filePath) {
+        File file = new File(filePath);
+        if (file.exists()) {
+            throw new RuntimeException("File already exists.");
+        }
+        try (FileWriter writer = new FileWriter(file)) {
             gson.toJson(this, writer);
         } catch (IOException e) {
-            e.printStackTrace();
+            throw new RuntimeException("Error saving deck: " + e.getMessage(), e);
         }
     }
 
-    public static User loadUserData(String filePath) {
-        Gson gson = new Gson();
-        try (FileReader reader = new FileReader(filePath)) {
-            return gson.fromJson(reader, User.class);
+    public void saveDeckByName(String name) {
+        if (savedDecks.containsKey(name)) {
+            throw new RuntimeException("Deck name already exists.");
+        }
+        String filePath = DECKS_DIR + name + ".json";
+        savedDecks.put(name, filePath);
+        saveDeckToFile(filePath);
+    }
+
+    public void loadDeckFromFile(String filePath) {
+        File file = new File(filePath);
+        if (!file.exists()) {
+            throw new RuntimeException("File does not exist.");
+        }
+        try (FileReader reader = new FileReader(file)) {
+            User loadedUser = gson.fromJson(reader, User.class);
+            this.factionSelected = loadedUser.factionSelected;
+            this.factionLeader = loadedUser.factionLeader;
+            this.commonCardsInDeck.clear();
+            this.commonCardsInDeck.addAll(loadedUser.commonCardsInDeck);
+            this.specialCardsInDeck.clear();
+            this.specialCardsInDeck.addAll(loadedUser.specialCardsInDeck);
         } catch (IOException e) {
-            e.printStackTrace();
-            return null;
+            throw new RuntimeException("Error loading deck: " + e.getMessage(), e);
         }
     }
+
+    public void loadDeckByName(String name) {
+        if (!savedDecks.containsKey(name)) {
+            throw new RuntimeException("No deck saved with this name.");
+        }
+        String filePath = savedDecks.get(name);
+        loadDeckFromFile(filePath);
+    }
+
 }
